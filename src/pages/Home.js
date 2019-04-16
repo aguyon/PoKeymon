615a4e75b7b0d967557191fb8e7a59e0698c53de
Virgile Vercellis <vmv92i@hotmail.fr>
2019-04-16 16:46:59 +0200

    Homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
+import bulbasaur from '../media/001-bulbasaur.svg';
+import charmander from '../media/004-charmander.svg';
+import squirtle from '../media/007-squirtle.svg';
+import '../css/Home.css';
 
 class Home extends Component {
   constructor() {
@@ -25,7 +29,19 @@ class Home extends Component {
             <NavLink to="/pokedex">PoKeydex</NavLink>
           </Nav.Item>
         </Nav>
+
+
         <h2>Accueil</h2>
+        <h3>Choose your first Pokemon</h3>
+        <img className="ImageAcceuil" src={bulbasaur} alt="Bulbizarre" />
+        <img className="ImageAcceuil" src={charmander} alt="Salameche" />
+        <img className="ImageAcceuil" src={squirtle} alt="Carapuce" />
+        <br />
+
+        <Nav>
+          <NavLink to="/solo-mode" className="ModeHomePage2">Solo Mode</NavLink>
+          <NavLink to="/dual-mode" className="ModeHomePage2">Dual Mode</NavLink>
+        </Nav>  
       </div>
     );
   }
