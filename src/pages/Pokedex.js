9d335ed62011426f3df69b22c6a712635f192154
aguyon <alexisguyon01@gmail.com>
2019-04-17 13:55:57 +0200

    info pokemon

--- src/pages/Pokedex.js
+++ src/pages/Pokedex.js
@@ -1,33 +1,38 @@
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import React from 'react';
+import {
+  HashRouter as Router, Route, Switch, NavLink,
+} from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 
-class Pokedex extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
+import Pokemon from '../components/Pokemon';
+import PokemonList from '../components/PokemonList';
 
-  render() {
-    return (
-      <div>
-        <Nav className="justify-content-center">
-          <Nav.Item>
-            <NavLink to="/">Accueil</NavLink>
-          </Nav.Item>
-          <Nav.Item>
-            <NavLink to="/solo-mode">Solo Mode</NavLink>
-          </Nav.Item>
-          <Nav.Item>
-            <NavLink to="/dual-mode">Dual Mode</NavLink>
-          </Nav.Item>
-          <Nav.Item>
-            <NavLink to="/pokedex" activeClassName="selected">PoKeydex</NavLink>
-          </Nav.Item>
-        </Nav>
-      </div>
-    );
-  }
-}
+import '../css/Pokedex.css';
+
+
+const Pokedex = () => (
+  <div>
+    <Nav className="justify-content-center">
+      <Nav.Item>
+        <NavLink to="/">Accueil</NavLink>
+      </Nav.Item>
+      <Nav.Item>
+        <NavLink to="/solo-mode">Solo Mode</NavLink>
+      </Nav.Item>
+      <Nav.Item>
+        <NavLink to="/dual-mode">Dual Mode</NavLink>
+      </Nav.Item>
+      <Nav.Item>
+        <NavLink to="/pokedex" activeClassName="selected">PoKeydex</NavLink>
+      </Nav.Item>
+    </Nav>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={PokemonList} />
+        <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />
+      </Switch>
+    </Router>
+  </div>
+);
 
 export default Pokedex;
