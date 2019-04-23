cf73be00119d5ea0e3779df0ff090373361a4e44
Lucas Blamont <lblamont@mac.com>
2019-04-23 11:12:02 +0200

    login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
+import { browserHistory } from 'react-router';
 import '../css/Login.css';
 
 export default class Login extends Component {
+  // eslint-disable-next-line class-methods-use-this
+  onNavigateHome() {
+    browserHistory.push('/solo-mode');
+  }
+
   render() {
     return (
       <div className="Login">
@@ -13,8 +19,9 @@ export default class Login extends Component {
           <input type="text" name="user" style={{ marginBottom: '20px', width: '50%', borderRadius: '5px' }} />
 
           <Button
+            onClick={this.onNavigateHome}
+            className="btn btn-primary"
             block
-            bsSize="large"
             type="submit"
           >
             Catch them all
