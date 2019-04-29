7ed1172e3b67c959c746c41e4be25c0940f40109
Lucas Blamont <lblamont@mac.com>
2019-04-29 14:49:54 +0200

    MAJ LOGIN

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,34 +4,47 @@ import { browserHistory } from 'react-router';
 import '../css/Login.css';
 
 export default class Login extends Component {
-  // eslint-disable-next-line class-methods-use-this
-  onNavigateHome() {
-    browserHistory.push('/');
+  constructor(props) {
+    super(props);
+
+    this.state = { name: '' };
   }
 
-  render() {
-    return (
-      <div className="Login">
-        <div id="joint-the-game">
-          <h1> Join the game </h1>
-        </div>
-        <div id="text-user">
-          Enter your name
-        </div>
-        <form onSubmit={this.handleSubmit}>
-          <div id="text-login">
-            <input type="text" name="user" style={{ marginBottom: '20px', width: '50%', borderRadius: '5px' }} />
-          </div>
-          <Button
-            onClick={this.onNavigateHome}
-            className="btn btn-primary"
-            block
-            type="submit"
-          >
-            Catch them all
-          </Button>
-        </form>
-      </div>
-    );
+  handleChange = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
   }
+
+    onNavigateHome = () => {
+      const { name } = this.state;
+      browserHistory.push('/');
+      localStorage.setItem('userActive', name);
+    }
+
+    render() {
+      return (
+        <div className="Login">
+          <div id="joint-the-game">
+            <h1> Join the game </h1>
+          </div>
+          <div id="text-user">
+            Enter your name
+          </div>
+          <form onSubmit={this.handleSubmit}>
+            <div id="text-login">
+              <input type="text" name="user" onChange={this.handleChange} style={{ marginBottom: '20px', width: '50%', borderRadius: '5px' }} />
+            </div>
+            <Button
+              onClick={this.onNavigateHome}
+              className="btn btn-primary"
+              block
+              type="submit"
+            >
+              Catch them all
+            </Button>
+          </form>
+        </div>
+      );
+    }
 }
